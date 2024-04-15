f74e0540c415e2cdc66a7720b12eff6fb2f1e7c2
Carlo M <carlo992@gmail.com>
2024-04-15 13:14:13 +0800

    Update Core.cs with more clarification

--- exercise.main/Core.cs
+++ exercise.main/Core.cs
@@ -11,16 +11,16 @@ namespace exercise.main
     public class Core
     {
        
+        // Note:
+        // IEnumerable is a class that the List inherits (ie List has all the functionality of IEnumerable)
+        // IEnumerable allows you to use a foreach loop on the variable hands (which is a List of tuples)
         
-        //TODO: complete the following method, keeping the signature the same
-        public bool winningPair(IEnumerable<Tuple<string, string>> hand, out Tuple<string, string> result)
+        //TODO: complete the winningPair method, without chaning the method signature
+        public bool winningPair(IEnumerable<Tuple<string, string>> hands, out Tuple<string, string> result)
         {
             result = new Tuple<string,string>(string.Empty, string.Empty);
-           
 
-          
-
-          
+            // your code here...          
 
             return result.Item1!=string.Empty ? true : false;
         }
