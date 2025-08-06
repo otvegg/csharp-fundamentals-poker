6031992a0d19bcb8bedf3d8dc783bf62c8bfe9c9
Oyvind Timian Dokk Husveg <oyvind.timian.dokk.husveg@no.experis.com>
2025-08-06 15:37:58 +0200

    Finished first implenetation, all tests passed.

--- exercise.main/Core.cs
+++ exercise.main/Core.cs
@@ -8,25 +8,81 @@ using System.Threading.Tasks;
 
 namespace exercise.main
 {
+
     public class Core
     {
-       
+
+        private readonly Dictionary<char, int> Cards = new Dictionary<char, int> {
+            { 'J', 11 },
+            { 'Q', 12 },
+            { 'K', 13 },
+            { 'A', 14 },
+        };
+
         // Note:
         // IEnumerable is a class that the List inherits (ie List has all the functionality of IEnumerable)
         // IEnumerable allows you to use a foreach loop on the variable hands (which is a List of tuples)
         
-        //TODO: complete the winningPair method, without chaning the method signature
+        //TODO: complete the winningPair method, without changing the method signature
         public bool winningPair(IEnumerable<Tuple<string, string>> hands, out Tuple<string, string> result)
         {
+
             result = new Tuple<string,string>(string.Empty, string.Empty);
 
-            // your code here...          
+            int BiggestPair = 0;
+            foreach (var pair in hands)
+            {
+                //string[] cards = pair.ToString().Trim('(').Trim(')').Split(",");
+                if (pair.Item1 == pair.Item2 && pair.Item1 != null)
+                {
+                    int i = 0;
+                    bool isInt = int.TryParse(pair.Item1, out i);
+                    if (isInt) 
+                    { 
+                        if (BiggestPair < i) BiggestPair = i; 
+                    }
+                    else
+                    {
+                        char temporaryCard = char.Parse(pair.Item1);
+                        if (BiggestPair < Cards[temporaryCard]) BiggestPair = Cards[temporaryCard];
+                    }
+                }
+            }
 
-            return result.Item1!=string.Empty ? true : false;
+            string BiggestCard = String.Empty;
+            if (BiggestPair > 10)
+            {
+                foreach (var card in Cards)
+                {
+                    if (card.Value == BiggestPair)
+                    {
+                        BiggestCard = card.Key.ToString();
+                        result = new Tuple<string, string>(BiggestCard, BiggestCard);
+                        break;
+                    }
+                }
+            }
+            else if (BiggestPair > 0)
+            {
+                BiggestCard = BiggestPair.ToString();
+                result = new Tuple<string, string>(BiggestCard, BiggestCard);
+            }
+
+                return BiggestPair != 0 ? true : false;
         }
         public int GetValueOfCard(string card)
         {
-            return  0;           
+            int i = 0;
+            bool isInt = int.TryParse(card, out i);
+            if (isInt)
+            {
+                return i;
+            }
+            else
+            {
+                char temporaryCard = char.Parse(card);
+                return Cards[temporaryCard];
+            }
         }
     }
 }
--- exercise.main/Program.cs
+++ exercise.main/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using exercise.main;
+
 Console.WriteLine("Hello, World!");
+
+Core core = new Core();
+
+List<Tuple<string, string>> hand = new List<Tuple<string, string>>
+        {
+            new Tuple<string, string>("K", "5"),
+            new Tuple<string, string>("3","7")
+        };
+Tuple<string, string> winner;
+
+bool result = core.winningPair(hand, out winner);
